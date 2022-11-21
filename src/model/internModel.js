e9bfc887d530a020cda7bad6d516a0ddf6ab9dbf
Pooja-sisodia <poojasisodia406@gmail.com>
2022-11-21 13:32:02 +0530

    adding model and validator

---
+++ src/model/internModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = mongoose.Schema({ 
+    name: {
+        type:String,
+        required:true
+    }, 
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    }, 
+    mobile: {
+        type:Number,
+        required:true, 
+        unique:true
+    }, 
+    collegeId: {
+        type:ObjectId, 
+        ref:"College", 
+    },
+    isDeleted: {
+        type:Boolean, 
+        default: false
+    }
+},{ timestamps: true })
+
+module.exports = mongoose.model('Intern', internSchema)
+const mongoose=require("mongoose");
+
