ff0bd188bd5a1ab01165ea0e1f8061ed4ab07e62
Pooja-sisodia <poojasisodia406@gmail.com>
2022-11-21 13:22:25 +0530

    adding package.json file index and router file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const route = require('./router/router');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://kanirudh726:0cczXnVbYKHycDtp@project2-group14.tjfjpyg.mongodb.net/test" ,{
+    UseNewUrlParser: true
+})
+
+.then( () => console.log("mongoose is connected"))
+.catch((err) => (err.message))
+
+app.use('/' , route)
+
+app.listen(process.env.PORT ||3000, function(){
+    console.log("express app running on port "+(process.env.port||3000))
+})
