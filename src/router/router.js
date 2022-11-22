cf2b4162590451d70a4ec6f4281fcc9bc841817f
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-11-23 00:17:59 +0530

    final push

--- src/router/router.js
+++ src/router/router.js
@@ -4,9 +4,9 @@ const collegeController = require('../controller/collegeController');
 const internController = require('../controller/internController');
 
 //api
-router.post('/functionUp/colleges', collegeController.createCollege)
-router.post('/functionUp/Interns', internController.createInterns)
-router.get('/functionUp/Colleges', collegeController.collegeDetails)
+router.post('/functionup/colleges', collegeController.createCollege)
+router.post('/functionup/interns', internController.createInterns)
+router.get('/functionup/collegeDetails', collegeController.collegeDetails)
 router.all("/*",function(req,res){
     res.status(404).send({msg:"invalid http request"})
 })
